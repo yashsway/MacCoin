bb8cadee0d7b906e11b18eacbd2f377ae15cdc80
Jemar Jones <jemarkjones@gmail.com>
2018-03-22 13:01:19 -0400

    Started main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
 
 import '../styles/App.css';
 
 const App = () => (
-  <div>
-    Hello World!
+  <div className='main-page'>
+    <div className='heading-block'>
+      <h1 className='heading center'>
+        MACCOIN
+      </h1>
+      <p className='center subheading'>~funny stuff here~</p>
+    </div>
+    <div className='center btn-wrapper'>
+      <Button className='btn-mac' bsSize='large' href='#'>Start Mining!</Button>
+    </div>
+    <div className='center'>
+      <Button bsStyle='link' href='#'>What is MacCoin?</Button>
+    </div>
   </div>
 );
 
