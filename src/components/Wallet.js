0c28ba2253514fd6390e3062e788921184c91282
Jemar Jones <jemarkjones@gmail.com>
2018-03-22 17:51:46 -0400

    Working on modal

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import ReactModal from 'react-modal';
 
 import Nav from './Nav';
 
 import '../styles/Wallet.css';
 
+class Wallet extends Component {
+  constructor(props) {
+    super(props);
 
-const Wallet = ({wallet, balance, transactions}) => (
-  <div className='wallet-page'>
-    <Nav current='wallet'/>
-    <p className='center small-heading wallet-header'>Your Wallet:</p>
-    <div className='wallet-info center'>
-      <div className='info-box'>id: {'bvger'}</div>
-      <div className='info-box'>balance: {78324}</div>
-      <Button className='btn-mac' bsSize='large'>Send</Button>
-    </div>
-    <p className='center small-heading wallet-header'>Transaction History:</p>
-    <div className='center transactions-container'>
-      <div className='info-box'>Recieved 400m from kda29 on Friday, March 23, 3:00pm</div>
-      <div className='info-box'>Sent 40m to kda29 on Friday, March 23, 3:00pm</div>
-      <div className='info-box'>Recieved 400m from kjnf29 on Friday, March 23, 3:00pm</div>
-      <div className='info-box'>Recieved 400m from kda29 on Friday, March 23, 3:00pm</div>
-      <div className='info-box'>Sent 40m to kda29 on Friday, March 23, 3:00pm</div>
-      <div className='info-box'>Recieved 400m from kjnf29 on Friday, March 23, 3:00pm</div>
-    </div>
-  </div>
-);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
+  toggleModal() {
+    this.setState({modalOpen: !this.props.modalOpen});
+  }
+
+  render() {
+    const { modalOpen } = this.state;
+
+    return (
+      <div className='wallet-page'>
+        <Nav current='wallet'/>
+        <p className='center small-heading wallet-header'>Your Wallet:</p>
+        <div className='wallet-info center'>
+          <div className='info-box'>id: {'bvger'}</div>
+          <div className='info-box'>balance: {78324}m</div>
+          <Button className='btn-mac' bsSize='large' onClick={() => this.toggleModal()}>Send</Button>
+          <ReactModal
+             isOpen={modalOpen}
+             contentLabel='Minimal Modal Example'>
+            
+          </ReactModal>
+        </div>
+        <p className='center small-heading wallet-header'>Transaction History:</p>
+        <div className='center transactions-container'>
+          <div className='info-box'>Recieved 400m from kda29 on Friday, March 23, 3:00pm</div>
+          <div className='info-box'>Sent 40m to kda29 on Friday, March 23, 3:00pm</div>
+          <div className='info-box'>Recieved 400m from kjnf29 on Friday, March 23, 3:00pm</div>
+          <div className='info-box'>Recieved 400m from kda29 on Friday, March 23, 3:00pm</div>
+          <div className='info-box'>Sent 40m to kda29 on Friday, March 23, 3:00pm</div>
+          <div className='info-box'>Recieved 400m from kjnf29 on Friday, March 23, 3:00pm</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Wallet;
