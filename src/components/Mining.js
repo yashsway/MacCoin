6aa0e8c9a1d8899f9095f2354ce9a3171b03968f
Jemar Jones <jemarkjones@gmail.com>
2018-03-22 15:54:29 -0400

    Added mining page template

--- src/components/Mining.js
+++ src/components/Mining.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import Nav from './Nav';
 
 import '../styles/Mining.css';
 
-const Mining = () => (
+const Mining = ({balance}) => (
   <div className='mining-page'>
     <Nav current='mining'/>
+    <div className='balance-container'>
+      <p className='center small-heading mining-text'>Mining:</p>
+      <div className='center'>
+        <img className='mining-gif' src='https://media.giphy.com/media/cnCnU42hrY0Ew/giphy.gif'></img>
+      </div>
+      <p className='center small-heading'>Balance: {3874}m</p>
+    </div>
+    <div className='center'>
+      <LinkContainer to='/wallet'>
+        <Button bsStyle='link'>Go to wallet</Button>
+      </LinkContainer>
+    </div>
   </div>
 );
 
