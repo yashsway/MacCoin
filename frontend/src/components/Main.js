c3908c2599af1325cf3c6e8d6b08d15ac9e35ff0
Jemar Jones <jemarkjones@gmail.com>
2018-03-23 01:19:09 -0400

    Cycling through quotes courtesy of daniel

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import '../styles/Main.css';
 
-const Main = () => (
-  <div className='main-page'>
-    <div className='heading-block'>
-      <h1 className='heading center'>
-        MACCOIN
-      </h1>
-      <p className='center small-heading'>~funny stuff here~</p>
-    </div>
-    <div className='center btn-wrapper'>
-      <LinkContainer to='/mining'>
-        <Button className='btn-mac' bsSize='large'>Start Mining!</Button>
-      </LinkContainer>
-    </div>
-    <div className='center'>
-      <LinkContainer to='/about'>
-        <Button bsStyle='link'>What is MacCoin?</Button>
-      </LinkContainer>
-    </div>
-  </div>
-);
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.quotes = ['“No personal information required” - Mark Zuckerberg', '“A holistic coinage solution” - Steve Jobs', '“Donate to Malaria relief” - Bill Gates', '"01001100 01001001 01001110 01010101 01011000" - Linus Torvalds', '“Give this to me instead”. - Gabe Newell', '“Equivalent in value to an 80 hour work week” - Jeff Bezos', '“I actually coded this, these damn students stole credit for my work” - Steve “The Woz” Wozniak', '“Why didn’t I think of this?” - Satoshi'];
+    this.state = {
+      quote: this.quotes[Math.floor(Math.random() * this.quotes.length)]
+    };
+  }
+
+  render() {
+    const { quote } = this.state;
+
+    return (
+      <div className='main-page'>
+        <div className='heading-block'>
+          <h1 className='heading center'>
+            MACCOIN
+          </h1>
+          <p className='center small-heading quotes'>{quote}</p>
+        </div>
+        <div className='center btn-wrapper'>
+          <LinkContainer to='/mining'>
+            <Button className='btn-mac' bsSize='large'>Start Mining!</Button>
+          </LinkContainer>
+        </div>
+        <div className='center'>
+          <LinkContainer to='/about'>
+            <Button bsStyle='link'>What is MacCoin?</Button>
+          </LinkContainer>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Main;
